f2039fdabb29f06afafbb44e0a027d21193cd205
j1531 <j1531@matsue-ct.jp>
2018-11-13 15:39:00 +0900

    add test1.rb

---
+++ test1.rb
@@ -0,0 +1,12 @@
+(1..100).each{|i|
+    if i%3 == 0 && i%5 == 0 then
+        print("FizzBuzz\n")
+    elsif i%3 == 0 then
+        print("Fizz\n")
+    elsif i%5 == 0 then
+        print("Buzz\n")
+    else
+        print("i\n")
+}
+
+
